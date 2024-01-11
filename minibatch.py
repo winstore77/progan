af534a28dab34f066e55ae28fc45e3c227d96673
winstore77 <152374136+winstore77@users.noreply.github.com>
2024-01-11 12:20:35 +0900

    Create minibatch.py

---
+++ minibatch.py
@@ -0,0 +1,26 @@
+# mini-batch standard deviation layer
+class MinibatchStdev(Layer):
+    # initialize the layer
+    def __init__(self, **kwargs):
+        super(MinibatchStdev, self).__init__(**kwargs)
+ 
+    # perform the operation
+    def call(self, inputs):
+        mean = backend.mean(inputs, axis=0, keepdims=True)
+        squ_diffs = backend.square(inputs - mean)
+        mean_sq_diff = backend.mean(squ_diffs, axis=0, keepdims=True)
+        mean_sq_diff += 1e-8
+        stdev = backend.sqrt(mean_sq_diff)
+        
+        mean_pix = backend.mean(stdev, keepdims=True)
+        shape = backend.shape(inputs)
+        output = backend.tile(mean_pix, (shape[0], shape[1], shape[2], 1))
+        
+        combined = backend.concatenate([inputs, output], axis=-1)
+        return combined
+ 
+    # define the output shape of the layer
+    def compute_output_shape(self, input_shape):
+        input_shape = list(input_shape)
+        input_shape[-1] += 1
+        return tuple(input_shape)
